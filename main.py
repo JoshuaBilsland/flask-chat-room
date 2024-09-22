2c4a3d129c3756e3221281bf5246d46fa7be8bf5
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2024-09-22 14:15:45 +0100

    feat: add chat room page

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+import random
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, join_room, leave_room, send
 
 app = Flask(__name__)
+socketio = SocketIO(app)  # Create server
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if "create" in request.form:
+        return render_template("chat.html")
     return render_template("home.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app)
